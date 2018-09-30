196e3c66f3031a103a4ba424aea20eab609763e9
Bhavin Patel <mailmebhav@gmail.com>
2018-09-30 17:18:17 +0530

    try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/apps',
+    path: '/admin',
+    redirectTo: '',
     pathMatch: 'full' }
   ];
 
